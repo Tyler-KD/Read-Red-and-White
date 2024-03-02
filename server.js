3df2017b7ec6f179e533c5cbd28a1757ab50ebe8
Jpalomo <Jpalomo92@gmail.com>
2024-03-02 15:41:37 -0600

    added json user data to userData.JSON

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Import routes
