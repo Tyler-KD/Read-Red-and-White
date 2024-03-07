c9336746a00bc11738a34fcfadd25c6c65c8a97b
Jpalomo <Jpalomo92@gmail.com>
2024-03-06 19:19:33 -0600

    linked bookapi js to the homepage

--- public/js/bookapi.js
+++ public/js/bookapi.js
@@ -1,22 +1,38 @@
-const API = 'AIzaSyBAVSXjnDVfRiLkrOIeNPqSADXVSBKJb9k'
-const url = `https://www.googleapis.com/books/v1/volumes?q=time&printType=books&key=AIzaSyBAVSXjnDVfRiLkrOIeNPqSADXVSBKJb9k`;
-const options = {
-    method: 'GET'
-}
+fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699")
+.then(function(res) {
+    return res.json();
+})
+.then(function(result) {
+    title = result.items[0].volumeInfo.title;
+    description = result.items[0].volumeInfo.description;
+    console.log(title);
+    console.log(description);
+})
+.then(function(error) {
+    console.log(error);
+})
 
 
 
-function fetchdata() {
-    fetch(url)
-
-    console.log(data)
-};
-
-fetchdata();
+// const API = 'AIzaSyBAVSXjnDVfRiLkrOIeNPqSADXVSBKJb9k'
+// const url = `https://www.googleapis.com/books/v1/volumes?q=time&printType=books&key=AIzaSyBAVSXjnDVfRiLkrOIeNPqSADXVSBKJb9k`;
+// const options = {
+//     method: 'GET'
+// }
 
 
-// function getApi() {
-//     const requestUrl = `GET https://www.googleapis.com/books/v1/volumes?q=time&printType=books&key=AIzaSyBAVSXjnDVfRiLkrOIeNPqSADXVSBKJb9k`;
+
+// function fetchdata() {
+//     fetch(url)
+
+//     console.log(data)
+// };
+
+// fetchdata();
+
+
+// // function getApi() {
+// //     const requestUrl = `GET https://www.googleapis.com/books/v1/volumes?q=time&printType=books&key=AIzaSyBAVSXjnDVfRiLkrOIeNPqSADXVSBKJb9k`;
 
 //     fetch(requestUrl)
 //      console.log(data);
